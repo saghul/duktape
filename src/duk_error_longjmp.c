3d63def11752df75c154ccbf2be5dd0579072720
Sami Vaarala <sami.vaarala@iki.fi>
2016-06-05 23:44:24 +0300

    More verbose uncaught error fatal message

--- src/duk_error_longjmp.c
+++ src/duk_error_longjmp.c
@@ -5,6 +5,47 @@
 
 #include "duk_internal.h"
 
+#if 0
+DUK_LOCAL void duk__uncaught_minimal(duk_hthread *thr) {
+	duk_fatal((duk_context *) thr, "uncaught error");
+}
+
+DUK_LOCAL void duk__uncaught_readable(duk_hthread *thr) {
+	const char *summary;
+	char buf[64];
+
+	summary = duk_push_string_tval_readable((duk_context *) thr, &thr->heap->lj.value1);
+	DUK_SNPRINTF(buf, sizeof(buf), "uncaught: %s", summary);
+	buf[sizeof(buf) - 1] = (char) 0;
+	duk_fatal((duk_context *) thr, (const char *) buf);
+}
+#endif
+
+DUK_LOCAL void duk__uncaught_error_aware(duk_hthread *thr) {
+	duk_context *ctx;
+	duk_tval *tv;
+	const char *summary;
+	char buf[64];
+
+	ctx = (duk_context *) thr;
+
+	/* Get error message in a side effect free way.  If that's not possible,
+	 * fall back to summarizing the uncaught error.
+	 */
+	duk_push_tval(ctx, &thr->heap->lj.value1);
+	if (duk_is_error(ctx, -1)) {
+		tv = duk_hobject_find_existing_entry_tval_ptr(thr->heap, duk_require_hobject(ctx, -1), DUK_HTHREAD_STRING_MESSAGE(thr));
+		if (tv) {
+			duk_push_tval(ctx, tv);
+		}
+	}
+
+	summary = duk_push_string_readable((duk_context *) thr, -1);
+	DUK_SNPRINTF(buf, sizeof(buf), "uncaught: %s", summary);
+	buf[sizeof(buf) - 1] = (char) 0;
+	duk_fatal((duk_context *) thr, (const char *) buf);
+}
+
 DUK_INTERNAL void duk_err_longjmp(duk_hthread *thr) {
 	DUK_ASSERT(thr != NULL);
 
@@ -26,7 +67,11 @@ DUK_INTERNAL void duk_err_longjmp(duk_hthread *thr) {
 		                 (int) thr->heap->lj.type, (int) thr->heap->lj.iserror,
 		                 &thr->heap->lj.value1, &thr->heap->lj.value2));
 
-		duk_fatal((duk_context *) thr, "uncaught error");
+#if 0
+		duk__uncaught_minimal(thr);
+		duk__uncaught_readable(thr);
+#endif
+		duk__uncaught_error_aware(thr);
 		DUK_UNREACHABLE();
 	}
 #endif  /* DUK_USE_CPP_EXCEPTIONS */
